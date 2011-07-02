689b2803eb47604ba968d1454cadf523b62fdbc4
Markus Kaiserswerth <mkai@sensun.org>
2011-07-02 02:23:54 +0200

    Renamed project

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='django-simple-follower',
-      version='0.1',
-      description="Simple Django follower/ following implementation taken and adapted from ericflo's starkthedark project.",
+setup(name='django-followers',
+      version='0.2',
+      description="Simple follow/ unfollow implementation for users, adapted from ericflo's starkthedark project.",
       long_description="",
       author='Markus Kaiserswerth',
       author_email='mkai@sensun.org',
       license='GPL',
-      packages=['simple_follower'],
+      packages=['followers'],
       zip_safe=False,
       install_requires=['django'],
 )
